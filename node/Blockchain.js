06a73125ebbff0b437b1ad0a287654602854669a
DMZPATH <dmzpath@patriciodimuzio.com.ar>
2019-03-05 15:00:58 -0300

    Working!

--- node/Blockchain.js
+++ node/Blockchain.js
@@ -4,18 +4,28 @@ class Block {
 		this.index = index;
 		this.timestamp = timestamp;
 		this.previousHash = previousHash;
-
+    this.data = data;
 		this.hash = this.calculateHash();
+		this.nonce = 0;
 	}
 
 	calculateHash(){
-		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+	}
+
+	mineBlock(difficulty) {
+		while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+			this.nonce++;
+			this.hash = this.calculateHash();
+		}
+		console.log("Block mined: " + this.hash);
 	}
 }
 
 class Blockchain {
 	constructor(){
 		this.chain = [this.createGenesisBlock()];
+		this.difficulty = 4;
 	}
 
 	createGenesisBlock(){
@@ -29,7 +39,7 @@ class Blockchain {
 	addBlock(newBlock){
 		newBlock.previousHash = this.getLatestBlock().hash;
 
-		newBlock.hash = newBlock.calculateHash();
+		newBlock.mineBlock(this.difficulty);
 		this.chain.push(newBlock);
 	}
 
@@ -52,14 +62,10 @@ class Blockchain {
 }
 
 let ourCrypto = new Blockchain();
-ourCrypto.addBlock(new Block(1,'10/07/2017',{amount: 4}));
-ourCrypto.addBlock(new Block(2,'1/15/2018',{amount: 10}));
+console.log("MinningBlock 1");
+ourCrypto.addBlock(new Block(1, "1/01/2018", {amount: 5}));
+console.log("MinningBlock 2");
+ourCrypto.addBlock(new Block(2, "1/01/2019", {amount: 32}));
 
 console.log(JSON.stringify(ourCrypto, null, 4));
-
-console.log('Is our blockchain valid?' + ourCrypto.isChainValid());
-
-ourCrypto.chain[1].data = {amount: 100};
-ourCrypto.chain[1].hash = ourCrypto.chain[1].calculateHash();
-
-console.log('Is our blockchain valid?' + ourCrypto.isChainValid());
+console.log('Is blockchain valid?' + ourCrypto.isChainValid().toString());
